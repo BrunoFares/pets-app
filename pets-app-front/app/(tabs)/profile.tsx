8d5c02aba9918753d2554c9b9aa3a5d7f076bdde
brunofares <faresbruno04@gmail.com>
2025-09-12 17:46:33 +0300

    Started working on the profile screen.

--- pets-app-front/app/(tabs)/profile.tsx
+++ pets-app-front/app/(tabs)/profile.tsx
@@ -1,33 +1,166 @@
 import { AdaptiveText } from "@/components/AdaptiveText";
 import { AdaptiveView } from "@/components/AdaptiveView";
-import { MainHeader } from "@/components/MainHeader";
 import { colors } from "@/constants/colors";
-import { StyleSheet, useColorScheme } from "react-native";
+import { useHeaderSlide } from "@/hooks/useHeaderSlide";
+import { AntDesign } from "@expo/vector-icons";
+import { useState } from "react";
+import {
+  Animated,
+  FlatList,
+  Image,
+  StyleSheet,
+  TouchableOpacity,
+  useColorScheme,
+  View,
+} from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 export default function Profile() {
-  const darkMode = useColorScheme() === 'dark';
-  const styles = createStyles({ darkMode });
+  const { translateY } = useHeaderSlide({ height: 200, duration: 200 });
+  const darkMode = useColorScheme() === "dark";
+  const styles = createStyles({ darkMode, translateY });
+  const [profileInfo, setProfileInfo] = useState<any>({
+    pets: [
+      {
+        key: 1,
+        name: "Kalinka",
+      },
+      {
+        key: 2,
+        name: "Minouche",
+      },
+    ],
+  });
 
   return (
     <SafeAreaView style={styles.container}>
-      <MainHeader />
+      <FlatList
+        data={profileInfo && profileInfo.pets ? profileInfo.pets : {}}
+        keyExtractor={(item) => String(item.key)}
+        ListHeaderComponent={
+          <View style={{ width: 380}}>
+            <Animated.View style={styles.head}>
+              <View>
+                {profileInfo && profileInfo.picture ? (
+                  <Image source={profileInfo.picture} />
+                ) : (
+                  <View style={styles.pfp} />
+                )}
+              </View>
+              <AdaptiveView>
+                <AdaptiveText style={styles.title}>Bruno</AdaptiveText>
+                <TouchableOpacity style={styles.editProfile}>
+                  <AdaptiveText style={styles.editProfileText}>
+                    Edit Profile
+                  </AdaptiveText>
+                </TouchableOpacity>
+              </AdaptiveView>
+            </Animated.View>
 
-      <AdaptiveView style={styles.body}><AdaptiveText>Profile</AdaptiveText></AdaptiveView>
+            <AdaptiveText
+              style={{
+                fontFamily: "Poppins-Bold",
+                fontSize: 18,
+                marginLeft: 20,
+                marginVertical: 15,
+              }}
+            >
+              My Pets
+            </AdaptiveText>
+
+            <TouchableOpacity style={styles.addPet}>
+              <AntDesign
+                name="plus"
+                size={30}
+                color={darkMode ? colors.white : colors.black}
+              />
+              <AdaptiveText style={styles.addPetText}>Add Pet</AdaptiveText>
+            </TouchableOpacity>
+          </View>
+        }
+        renderItem={({ item }) => {
+          return (
+            <TouchableOpacity style={styles.petListItem}>
+              <AdaptiveText style={styles.addPetText}>{item.name}</AdaptiveText>
+              <AntDesign
+                name="arrowright"
+                size={24}
+                color={darkMode ? colors.white : colors.black}
+              />
+            </TouchableOpacity>
+          );
+        }}
+      />
     </SafeAreaView>
   );
 }
 
-const createStyles = ({ darkMode }: any) => {
+const createStyles = ({ darkMode, translateY }: any) => {
   return StyleSheet.create({
     container: {
       flex: 1,
       backgroundColor: darkMode ? colors.veryDarkGrey : colors.white,
+      alignItems: "center",
     },
-    body: {
-      flex: 1,
-      alignItems: 'center',
-      justifyContent: 'center',
+    head: {
+      transform: [{ translateY }],
+      alignItems: "center",
+      justifyContent: "flex-start",
+      flexDirection: "row",
+      paddingVertical: 20,
+      paddingHorizontal: 16,
+      gap: 20,
     },
-  })
+    body: {},
+    title: {
+      fontFamily: "Poppins-SemiBold",
+      fontSize: 24,
+    },
+    editProfile: {
+      backgroundColor: darkMode ? colors.darkGrey : colors.lightGrey,
+      paddingHorizontal: 16,
+      paddingVertical: 4,
+      borderRadius: 10,
+    },
+    editProfileText: {
+      fontFamily: "Poppins-Regular",
+      fontSize: 16,
+    },
+    pfp: {
+      height: 120,
+      width: 120,
+      borderRadius: 120,
+      backgroundColor: colors.lightGrey,
+    },
+    addPet: {
+      backgroundColor: darkMode ? colors.darkGrey : colors.lightGrey,
+      flexDirection: "row",
+      gap: 20,
+      alignItems: "center",
+      justifyContent: "center",
+      width: "95%",
+      paddingVertical: 15,
+      borderRadius: 20,
+      alignSelf: "center",
+    },
+    addPetText: {
+      fontFamily: "Poppins-Medium",
+      fontSize: 20,
+      textAlign: "center",
+    },
+    petListItem: {
+      flexDirection: "row",
+      gap: 20,
+      alignItems: "center",
+      alignSelf: "center",
+      justifyContent: "space-between",
+      width: "95%",
+      paddingVertical: 15,
+      borderWidth: 1,
+      borderColor: darkMode ? colors.darkGrey : colors.lightGrey,
+      borderRadius: 20,
+      paddingHorizontal: 40,
+      marginTop: 10,
+    },
+  });
 };
