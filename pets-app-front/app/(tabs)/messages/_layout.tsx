c3b30b3e9d54882ea9624cc66fd79573caf3e817
brunofares <faresbruno04@gmail.com>
2026-05-03 05:24:39 +0300

    changed the app background to match with every screen's background color

--- pets-app-front/app/(tabs)/messages/_layout.tsx
+++ pets-app-front/app/(tabs)/messages/_layout.tsx
@@ -1,8 +1,18 @@
+import { colors } from "@/constants/colors";
 import { Stack } from "expo-router";
+import { useColorScheme } from "react-native";
 
 export default function MessagesStack() {
+  const darkMode = useColorScheme() === "dark";
+  const appBackground = darkMode ? colors.veryDarkGrey : colors.white;
+
   return (
-    <Stack screenOptions={{ headerShown: false }}>
+    <Stack
+      screenOptions={{
+        headerShown: false,
+        contentStyle: { backgroundColor: appBackground },
+      }}
+    >
       <Stack.Screen name="index" options={{ title: "DMs" }} />
       <Stack.Screen name="[id]" options={{ title: "Conversation" }} />
     </Stack>
